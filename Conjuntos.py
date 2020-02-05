3b79878cd2b650ee4fb1bb9c042489c4cee5b7a8
David Morales <david_ivan2402@ciencias.unam.mx>
2020-02-04 19:38:43 -0600

    hsfd

--- Conjuntos.py
+++ Conjuntos.py
@@ -20,14 +20,13 @@ def menu():
 
       while(bandera):
         entrada = int( input(''))
-  
         if entrada == 0:
           bandera = False
         else:
           conjunto1.append(entrada)
 
       # agregamos los elemetos al segundo conjunto
-      print('Ingresa los elementos del cojunto1: ')
+      print('Ingresa los elementos del cojunto2: ')
       print('Si deseas ya no agregar elementos pon 0')
   
       bandera = True
@@ -45,21 +44,46 @@ def menu():
     print('1. intersección')
     print('2. Union')
     print('3. Diferencia')
-    print('0. salir)
-
+    print('4. salir')
+    # Conjunto auxiliar donde almacenaremos nuestros datos
+    aux = []
     bandera = True
-    while():
+    while(bandera):
       entrada = int(input())
-       if (entrada == 1):
-         #relalizamos la intersección
-         
-        elif(entrada == 2):
-          # realizamos la union
-          for 
-
-        elif(entrada == 3):
-          # realizamos la diferencia
-
-        elif(entrada == 4):
+      if (entrada == 1):
+        #relalizamos la intersección
+        for i in range(len(conjunto1)):
+          if  i in conjunto2:
+            aux.append(i)   
+        bandera = False    
+      elif(entrada == 2):
+        # Operación union
+        #agregamos a nuestro cojunto auxiliar nuestro primeros elementos
+        for i in range(conjunto1):
+          if i not in aux:
+            aux.append(i)
+        bandera = False  
+        # Agregamos los elementos de nuestro segundo cojunto
+        for i in range(conjunto2):
+          if i not in aux:
+            aux.append(i)
           bandera = False
+
+      elif(entrada == 3):
+        # realizamos la diferencia para A/B
+        for i in range(len(conjunto1)):
+          if i not in conjunto2:
+            aux.append(i)
+        bandera = False
+        
+        
+
+      elif(entrada == 4):
+        bandera = False
+    
+    """
+      Imprimimos los elementos de nuestro arreglo auxiliar
+    """
+    for i in range(len(aux)):
+      print(i)
 menu()
